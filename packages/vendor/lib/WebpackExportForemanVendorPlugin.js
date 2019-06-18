b632d3bae1154916eb2d9fb08c7f59f690f4f35b
Avi Sharvit <asharvit@redhat.com>
2019-06-18 12:36:47 +0300

    fix(analyze): fix the npm run analyze

--- packages/vendor/lib/WebpackExportForemanVendorPlugin.js
+++ packages/vendor/lib/WebpackExportForemanVendorPlugin.js
@@ -3,8 +3,6 @@ const { default: InjectPlugin } = require('webpack-inject-plugin');
 function customLoader({ modules }) {
   const results = modules.map(module => module.createModuleExport()).join(' ');
 
-  console.log('results', results);
-
   return () => results;
 }
 
--- packages/vendor/lib/createVendorEntry.js
+++ packages/vendor/lib/createVendorEntry.js
@@ -3,8 +3,6 @@ const { modules } = require('@theforeman/vendor-core');
 const createVendorEntry = () => {
   const entry = ['./scss/vendor.scss', ...modules.map(module => module.path)];
 
-  console.log('entry', entry);
-
   return entry;
 };
 
