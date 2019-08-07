035eb1af34cb3716b0e847fa869911665587eae1
Avi Sharvit <asharvit@redhat.com>
2019-08-07 23:04:38 +0300

    fix(root): try to fix npm deploy

--- scripts/prepare-deploy-ci.sh
+++ scripts/prepare-deploy-ci.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-git config --global user.email "${GITHUB_EMAIL}"
-git config --global user.name "${GITHUB_NAME}"
+# configure npm user
+cp .npmrc-publish .npmrc
+
+# congigure github user
+git config --global user.email "${GH_EMAIL}"
+git config --global user.name "${GH_NAME}"
 git remote set-url origin https://${GH_TOKEN}@github.com/theforeman/foreman-js.git
 git fetch
 git checkout master
