a5ce42226286dfeb1df3e23b648f9718dc3c8e65
Avi Sharvit <asharvit@redhat.com>
2019-08-08 12:16:37 +0300

    fix(root): try to fix npm deploy

--- scripts/prepare-deploy-ci.sh
+++ scripts/prepare-deploy-ci.sh
@@ -5,13 +5,14 @@ set -e
 NPM_EMAIL=${NPM_EMAIL}
 NPM_USERNAME=${NPM_USERNAME}
 NPM_TOKEN=${NPM_TOKEN}
+NPM_REGISTRY="https://registry.npmjs.org/"
 GH_EMAIL=${GH_EMAIL}
 GH_NAME=${GH_NAME}
 GH_TOKEN=${GH_TOKEN}
 
 # configure npm user
 npm install --global npm-cli-login
-npm-cli-login -e $NPM_EMAIL -u $NPM_USERNAME -p $NPM_TOKEN
+npm-cli-login -e $NPM_EMAIL -u $NPM_USERNAME -p $NPM_TOKEN -r $NPM_REGISTRY
 
 # congigure github user
 git config --global user.email "${GH_EMAIL}"
