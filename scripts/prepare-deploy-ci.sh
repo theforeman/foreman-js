fb97ea3cccaab80e00e36576cf17fa435651a35e
Avi Sharvit <asharvit@redhat.com>
2019-08-08 08:13:14 +0300

    fix(root): try to fix npm deploy

--- scripts/prepare-deploy-ci.sh
+++ scripts/prepare-deploy-ci.sh
@@ -3,7 +3,7 @@
 set -e
 
 # configure npm user
-npm config set registry "//registry.npmjs.org/:_authToken=\\${NPM_TOKEN}"
+npm config set registry "https://registry.npmjs.org/:_authToken=\\${NPM_TOKEN}"
 
 # congigure github user
 git config --global user.email "${GH_EMAIL}"
