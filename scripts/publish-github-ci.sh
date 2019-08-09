b860f5b4e2410850e4949ee302d190008fd800d3
Avi Sharvit <asharvit@redhat.com>
2019-08-09 10:44:04 +0300

    fix(root): fix auto npm deploy

--- scripts/publish-github-ci.sh
+++ scripts/publish-github-ci.sh
@@ -2,10 +2,19 @@
 # Publish to github when pushing a new commit to master
 # Will create a new tagged commit and push to master
 
+npm run lerna -- changed --conventional-commits
+CHANGED=$?
+
 set -e
 
-npm run lerna -- version \
-  --conventional-commits \
-  --create-release github \
-  --message 'chore(root): publish %s' \
-  --yes
+if [ $CHANGED -eq 0 ]; then
+  echo "Changed detected, releasing a new version for all packages"
+
+  npm run lerna -- version \
+    --conventional-commits \
+    --create-release github \
+    --force-publish \
+    --yes
+else
+  echo "Skipping github publish"
+fi
