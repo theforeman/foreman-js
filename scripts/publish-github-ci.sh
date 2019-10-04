20cce39c987b221f8e61a2b273ab665180a24dd2
Avi Sharvit <sharvita@gmail.com>
2019-10-04 13:02:32 +0300

    fix(root): fix the autodeploy infi loop

--- scripts/publish-github-ci.sh
+++ scripts/publish-github-ci.sh
@@ -10,24 +10,11 @@ set -e
 if [ $CHANGED -eq 0 ]; then
   echo "Changed detected, releasing a new version for all packages"
 
-  # update the version
   npm run lerna -- version \
     --conventional-commits \
     --create-release github \
     --force-publish \
-    --no-push \
     --yes
-
-  # update the lock files
-  npm install
-  git add **/package-lock.json
-
-  # commit and push changes
-  git commit --amend --no-edit
-  VERSION=`node -pe "require('$PWD/package.json').version;"`
-  git tag -f "v${VERSION}"
-  git push --follow-tags
-  git push --tags
 else
   echo "Skipping github publish"
 fi
