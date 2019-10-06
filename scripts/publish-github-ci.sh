17053cf4357c038e7a8556a986830e3963252bf4
Avi Sharvit <sharvita@gmail.com>
2019-10-06 17:00:49 +0300

    fix(root): attempt to fix the lock files update in the CI

--- scripts/publish-github-ci.sh
+++ scripts/publish-github-ci.sh
@@ -2,15 +2,13 @@
 # Publish to github when pushing a new commit to master
 # Will create a new tagged commit and push to master
 
-CI_PUBLISH_GITHUB=true
-
 npm run lerna -- changed --conventional-commits
 CHANGED=$?
 
 set -e
 
 if [ $CHANGED -eq 0 ]; then
-  echo "Changed detected, releasing a new version for all packages"
+  echo "Detected relevant changes, releasing a new version for all packages."
 
   npm run lerna -- version \
     --conventional-commits \
@@ -19,5 +17,5 @@ if [ $CHANGED -eq 0 ]; then
     --commit-hooks \
     --yes
 else
-  echo "Skipping github publish"
+  echo "Detected no relevat changes, skipping github publish."
 fi
