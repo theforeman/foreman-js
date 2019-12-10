acf2f308163e90573341052320f5dcbefee036c1
Amir Fefer <amirfefer@gmail.com>
2019-12-10 19:56:35 +0200

    feat(env): adding jest

---
+++ packages/env/test/test_setup.js
@@ -0,0 +1,17 @@
+import 'core-js/shim';
+import 'regenerator-runtime/runtime';
+
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+configure({ adapter: new Adapter() });
+
+// https://github.com/facebook/jest/issues/6121
+// eslint-disable-next-line no-console
+const { error } = console;
+// eslint-disable-next-line no-console
+console.error = (message, ...args) => {
+  error.apply(console, args); // keep default behaviour
+  const err = message instanceof Error ? message : new Error(message);
+  throw err;
+};
