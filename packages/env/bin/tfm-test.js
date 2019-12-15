00be9ca28d42ede8fc45f41543b7317917a05d5a
Amir Fefer <amirfefer@gmail.com>
2019-12-15 14:26:22 +0200

    fix(env): fix bin path for tfm-test

---
+++ packages/env/bin/tfm-test.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const path = require('path');
+const { runScript } = require('../helpers');
+
+const envRoot = path.resolve(__dirname, '../');
+const configArg = ['--config', `${envRoot}/test/config.js`];
+const args = process.argv.slice(2).concat(configArg);
+
+const errorHandling = error => {
+  if (error) throw error;
+};
+
+runScript(`${envRoot}/node_modules/jest/bin/jest.js`, errorHandling, args);
---
+++ packages/env/helpers/index.js
@@ -0,0 +1,22 @@
+const childProcess = require('child_process');
+
+module.exports = {
+  runScript: (scriptPath, callback, ...args) => {
+    let invoked = false;
+
+    const process = childProcess.fork(scriptPath, args);
+
+    process.on('error', err => {
+      if (invoked) return;
+      invoked = true;
+      callback(err);
+    });
+
+    process.on('exit', code => {
+      if (invoked) return;
+      invoked = true;
+      const err = code === 0 ? null : new Error(`exit code ${code}`);
+      callback(err);
+    });
+  },
+};
