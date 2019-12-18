704d0e9c1c656aaae1f8691317caf789634196f1
Amir Fefer <amirfefer@gmail.com>
2019-12-18 13:27:32 +0200

    fix(env): fix tfm-test script

--- packages/builder/babel/presets.js
+++ packages/builder/babel/presets.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
+const path = require('path');
 
 const presets = [
   require.resolve('@babel/preset-env'),
   require.resolve('@babel/preset-react'),
 ];
 
-// Try to load `@theforeman/env/babel`
+// Try to load `@theforeman/env/babel` from the consumer
 try {
-  const { paths } = require.main;
-  const envPreset = require.resolve('@theforeman/env/babel', { paths });
+  const consumerNodeModulesPath = path.resolve(process.cwd(), './node_modules');
+
+  const envPreset = require.resolve('@theforeman/env/babel', {
+    paths: [consumerNodeModulesPath],
+  });
 
   presets.push(envPreset);
 } catch (error) {
--- packages/env/test/config.js
+++ packages/env/test/config.js
@@ -21,21 +21,25 @@ module.exports = {
   unmockedModulePathPatterns: ['react', 'node_modules/'],
   moduleNameMapper: {
     '^.+\\.(png|gif|css|scss)$': 'identity-obj-proxy',
+    '^dnd-core$': 'dnd-core/dist/cjs',
+    '^react-dnd$': 'react-dnd/dist/cjs',
+    '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
+    '^react-dnd-touch-backend$': 'react-dnd-touch-backend/dist/cjs',
+    '^react-dnd-test-backend$': 'react-dnd-test-backend/dist/cjs',
+    '^react-dnd-test-utils$': 'react-dnd-test-utils/dist/cjs',
   },
   globals: {
     __testing__: true,
     URL_PREFIX: '',
   },
-  transformIgnorePatterns: [
-    `/${cwd}/node_modules/(?!@theforeman/vendor-core/lib/)`,
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!@theforeman/vendor-core/lib/)'],
   transform: {
     '^.+\\.js$': '<rootDir>/test/jestPreprocess.js',
   },
   moduleDirectories: [
+    '<rootDir>/node_modules',
     `${cwd}/node_modules/@theforeman/vendor-core/node_modules`,
     `${cwd}/node_modules`,
-    '<rootDir>/node_modules',
   ],
   setupFiles: [
     'raf/polyfill',
