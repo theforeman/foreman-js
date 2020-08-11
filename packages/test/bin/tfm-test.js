209641ba42950d6e4691fd7f566f5b3d13623bcb
John Mitsch <jomitsch@redhat.com>
2020-08-11 13:23:58 +0000

    Update jest config to use find-foreman
    This will allow plugins to use foreman's node modules during jest tests. Also an option was added to override the standard configuration. This is useful when a plugin doesn't want to completely replace the config, rather override or add to the standard options.

--- packages/test/bin/tfm-test.js
+++ packages/test/bin/tfm-test.js
@@ -13,19 +13,31 @@ program.version(version);
 program
   .allowUnknownOption()
   .option('-p, --plugin', 'plugin support')
-  .option('-c, --config <type>', 'path to alternative config');
+  .option('-c, --config <type>', 
+          'path to alternative jest config that will be used instead of the standard config')
+  .option('-o --override-config <type>',
+          'path to jest config that will be used in addition to the standard config. This will ' + 
+          'override and add additional settings')
 
 program.parse(process.argv);
 
-const { plugin, config } = program;
+const { plugin, config, overrideConfig } = program;
 
-const overrideConfig = () => {
+const jestConfig = () => {
   if (config) return config;
-  return plugin
+  const standardConfig = plugin
     ? `${testRoot}/src/pluginConfig.js`
     : `${testRoot}/src/config.js`;
+  if (overrideConfig) {
+    const parsedStandard = require(standardConfig);
+    const parsedOverride = require(path.resolve(testRoot, overrideConfig));
+    // Jest accepts file names or JSON objects
+    const combinedConfig = JSON.stringify({ ...parsedStandard, ...parsedOverride });
+    return combinedConfig;
+  }
+  return standardConfig;
 };
-const configArg = ['--config', overrideConfig()];
+const configArg = ['--config', jestConfig()];
 const jestArgs = configArg.concat(remainingArgs(program));
 
 const errorHandling = error => {
--- packages/test/src/pluginConfig.js
+++ packages/test/src/pluginConfig.js
@@ -1,6 +1,23 @@
+const { foremanLocation, foremanRelativePath } = require('@theforeman/find-foreman')
 const config = require('./config');
+
+const foremanReactRelative = 'webpack/assets/javascripts/react_app';
+const foremanFull = foremanLocation();
+const foremanReactFull = foremanRelativePath(foremanReactRelative);
 
 module.exports = {
   ...config,
+  moduleDirectories: [
+    `${foremanFull}/node_modules`,
+    `${foremanFull}/node_modules/@theforeman/vendor-core/node_modules`,
+    'node_modules',
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/foreman/',
+  ],
+  moduleNameMapper: {
+    ...config.moduleNameMapper,
+    '^foremanReact(.*)$': `${foremanReactFull}/$1`,
+  },
   automock: false,
 };
