30acc1f5642b2b71f3ad08976ef42022b317c062
Jeremy Lenz <jlenz@redhat.com>
2020-09-23 16:45:55 -0400

    feat(stories): fix node.js cwd syntax

--- packages/stories/src/config/main.js
+++ packages/stories/src/config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: [`${process.env.CWD}/webpack/**/*.stories.@(js|mdx)`],
+  stories: [`${process.cwd()}/webpack/**/*.stories.@(js|mdx)`],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
