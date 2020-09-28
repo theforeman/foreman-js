5112ddabd88499dfd761dd3a442ec05cf1dfd366
Jeremy Lenz <jlenz@redhat.com>
2020-09-28 15:33:53 -0400

    feat(stories): update sorting function to work around sb issue

--- packages/stories/src/config/services/sorting.js
+++ packages/stories/src/config/services/sorting.js
@@ -6,13 +6,24 @@ export const storyWeight = 1000;
 
 /**
  * Compare 2 stories and sort them
+ * Negative return value means storyA is first; positive means storyB is first
  * @return {Number}
  */
 export const storySort = ([storyIdA, storyA], [storyIdB, storyB]) => {
   // do not sort stories from the same kind
   if (storyA.kind === storyB.kind) return 0;
 
-  // use parameters.storyWeight
+  // hard code Getting Started to show first
+  if (storyA.kind === 'Introduction/Getting Started') return -1;
+  if (storyB.kind === 'Introduction/Getting Started') return 1;
+
+  // show docs-only stories first
+  if (storyA.parameters.docsOnly && !storyB.parameters.docsOnly) {
+    return -1;
+  }
+  if (!storyA.parameters.docsOnly && storyB.parameters.docsOnly) return 1;
+
+  // use parameters.storyWeight - currently broken
   if (storyA.parameters.storyWeight !== storyB.parameters.storyWeight) {
     return storyA.parameters.storyWeight - storyB.parameters.storyWeight;
   }
