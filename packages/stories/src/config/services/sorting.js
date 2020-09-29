e2110de635befe2bfda63a0b2f4a7edc1b153c74
Jeremy Lenz <jlenz@redhat.com>
2020-09-29 09:38:10 -0400

    feat(stories): update storySort per v6 api

--- packages/stories/src/config/services/sorting.js
+++ packages/stories/src/config/services/sorting.js
@@ -7,26 +7,29 @@ export const storyWeight = 1000;
 /**
  * Compare 2 stories and sort them
  * Negative return value means storyA is first; positive means storyB is first
+ * This means lower-weighted stories go first in the sorting.
  * @return {Number}
  */
-export const storySort = ([storyIdA, storyA], [storyIdB, storyB]) => {
+export const storySort = (
+  [storyIdA, storyA, kindParamsA, globalParamsA],
+  [storyIdB, storyB, kindParamsB, globalParamsB]
+) => {
   // do not sort stories from the same kind
   if (storyA.kind === storyB.kind) return 0;
 
-  // hard code Getting Started to show first
-  if (storyA.kind === 'Introduction/Getting Started') return -1;
-  if (storyB.kind === 'Introduction/Getting Started') return 1;
+  // use storyWeight
+  const storyWeightA = kindParamsA.storyWeight || globalParamsA.storyWeight;
+  const storyWeightB = kindParamsB.storyWeight || globalParamsB.storyWeight;
+  const combined = storyWeightA - storyWeightB;
+  if (combined) {
+    return combined;
+  }
 
   // show docs-only stories first
   if (storyA.parameters.docsOnly && !storyB.parameters.docsOnly) {
     return -1;
   }
   if (!storyA.parameters.docsOnly && storyB.parameters.docsOnly) return 1;
-
-  // use parameters.storyWeight - currently broken
-  if (storyA.parameters.storyWeight !== storyB.parameters.storyWeight) {
-    return storyA.parameters.storyWeight - storyB.parameters.storyWeight;
-  }
 
   // use alphabet
   return storyIdA.localeCompare(storyIdB, { numeric: true });
