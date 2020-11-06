1b53640f2a74e0b17df14ccaec25b0475cdea1a3
Avi Sharvit <sharvita@gmail.com>
2020-11-06 15:16:27 +0200

    feat(root): migrate deploy to github actions
    drop travis and use github actions for deployment

--- scripts/publish-github-ci.sh
+++ scripts/publish-github-ci.sh
@@ -2,20 +2,40 @@
 # Publish to github when pushing a new commit to master
 # Will create a new tagged commit and push to master
 
+# required env variables
+GH_TOKEN=${GH_TOKEN}
+GITHUB_REF=${GITHUB_REF} # refs/heads/{branch-name}
+
+BRANCH_NAME=${GITHUB_REF#refs/heads/}
+LERNA_VERSION_ARGS=(
+  --conventional-commits
+  --create-release github
+  --force-publish
+  --commit-hooks
+  --yes
+)
+
+if [ $BRANCH_NAME == 'master' ]; then
+  NPM_TAG='latest'
+else
+  NPM_TAG=${BRANCH_NAME/\//-}
+  LERNA_VERSION_ARGS+=(
+    --conventional-prerelease
+    --preid $NPM_TAG
+  )
+fi
+
+# detect for changes
 npm run lerna -- changed --conventional-commits
 CHANGED=$?
 
 set -e
 
 if [ $CHANGED -eq 0 ]; then
-  echo "Detected relevant changes, releasing a new version for all packages."
+  echo "Detected relevant changes pushed to the $BRANCH_NAME branch!"
+  echo "Releasing a new version for all packages with $NPM_TAG tag."
 
-  npm run lerna -- version \
-    --conventional-commits \
-    --create-release github \
-    --force-publish \
-    --commit-hooks \
-    --yes
+  npm run lerna -- version ${LERNA_VERSION_ARGS[@]}
 else
   echo "Detected no relevat changes, skipping github publish."
 fi
--- scripts/set-git-user-to-foreman-js-bot-ci.sh
+++ scripts/set-git-user-to-foreman-js-bot-ci.sh
@@ -8,13 +8,7 @@ set -e
 # required env variables
 GH_EMAIL=${GH_EMAIL}
 GH_NAME=${GH_NAME}
-GH_TOKEN=${GH_TOKEN}
 
 # congigure github user
 git config --global user.email "${GH_EMAIL}"
 git config --global user.name "${GH_NAME}"
-git remote set-url origin https://${GH_TOKEN}@github.com/theforeman/foreman-js.git
-# bring all commits and tags
-git fetch
-git checkout master
-git rebase origin/master
