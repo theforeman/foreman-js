2823dabfe05985865ff2d39f299e0d784b732a4b
Avi Sharvit <sharvita@gmail.com>
2020-11-09 17:59:06 +0200

    fix(root): add npm-tag to npm publish

--- scripts/publish-npm-ci.sh
+++ scripts/publish-npm-ci.sh
@@ -3,4 +3,17 @@
 
 set -e
 
-npm run lerna -- publish from-package --yes
+# required env variables
+NPM_TOKEN=${NPM_TOKEN}
+GITHUB_REF=${GITHUB_REF} # refs/heads/{tag-name}
+
+VERSION=${GITHUB_REF#refs/heads/}
+REGEX_VERSION='v[0-9]+\.[0-9]+.[0-9]+-(.*)\.[0-9]+'
+
+NPM_TAG="latest"
+
+if [[ $VERSION =~ $REGEX_VERSION ]]; then
+  NPM_TAG=${BASH_REMATCH[1]}
+fi
+
+npm run lerna -- publish from-git --preid ${NPM_TAG} --yes
