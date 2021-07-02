4107d2d8008b200cef60d5f7147b5e9655c88fbe
Eric D. Helms <ericdhelms@gmail.com>
2021-07-02 16:28:28 -0400

    fix(builder): syntax optional chaining in builder
    Explicitly declare the plugin-syntax-optional-chaining babel plugin
    as well to ensure it is loaded.

--- packages/builder/babel/plugins.js
+++ packages/builder/babel/plugins.js
@@ -4,4 +4,5 @@ module.exports = [
   require.resolve('@babel/plugin-proposal-optional-chaining'),
   require.resolve('@babel/plugin-transform-object-assign'),
   require.resolve('@babel/plugin-syntax-dynamic-import'),
+  require.resolve('@babel/plugin-syntax-optional-chaining'),
 ];
