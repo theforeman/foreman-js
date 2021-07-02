259e6aeec7e789bb18b940d327a1292d4c66136d
Ondřej Ezr <oezr@redhat.com>
2021-07-02 14:40:39 +0200

    fix(builder): optional chaining in builder
    We are using optional chaining, but we do not include this in babel plugins, so on nodejs 10 this is
    failing to build.

--- packages/builder/babel/plugins.js
+++ packages/builder/babel/plugins.js
@@ -1,6 +1,7 @@
 module.exports = [
   require.resolve('@babel/plugin-proposal-class-properties'),
   require.resolve('@babel/plugin-proposal-object-rest-spread'),
+  require.resolve('@babel/plugin-proposal-optional-chaining'),
   require.resolve('@babel/plugin-transform-object-assign'),
   require.resolve('@babel/plugin-syntax-dynamic-import'),
 ];
