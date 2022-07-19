3a782426d7074f4dfef78e23368fc26eee9c5bd0
MariaAga <mariaaga@redhat.com>
2022-07-19 14:52:58 +0200

    fix(vendor): updating scss-bundle
    removing bootstrap mixins, replacing node-sass with sass
    
    BREAKING CHANGE:
    removing bootstrap mixins, replacing node-sass with sass

--- packages/vendor/scripts/build-scss.sh
+++ packages/vendor/scripts/build-scss.sh
@@ -6,11 +6,11 @@ NODE_ENV=production
 VENDOR_CORE_PATH=./node_modules/@theforeman/vendor-core
 
 # Build variables
-scss-bundle -e $VENDOR_CORE_PATH/scss/variables.scss \
-            -d scss/variables.scss \
-            --includePaths $VENDOR_CORE_PATH
+scss-bundle --entryFile $VENDOR_CORE_PATH/scss/variables.scss \
+            --outFile scss/variables.scss \
+            --project $VENDOR_CORE_PATH
 
 # Build mixins
-scss-bundle -e $VENDOR_CORE_PATH/scss/mixins.scss \
-            -d scss/mixins.scss \
-            --includePaths $VENDOR_CORE_PATH
+scss-bundle --entryFile $VENDOR_CORE_PATH/scss/mixins.scss \
+            --outFile scss/mixins.scss \
+            --project $VENDOR_CORE_PATH
